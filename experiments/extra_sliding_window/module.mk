1ce2d7abfe2f92df0c1ec99e24716b91360e41a5
Mohammad Agbarya <e.mohammad@gmail.com>
2022-09-07 23:34:33 +0300

    Add extra_sliding_window makefiles

---
+++ experiments/extra_sliding_window/module.mk
@@ -0,0 +1,10 @@
+EXTRA_SLIDING_WINDOW_MODULE_NAME := experiments/extra_sliding_window
+EXTRA_HOT_REGION_FILE := $(EXTRA_SLIDING_WINDOW_MODULE_NAME)/hot_region.txt
+EXTRA_SLIDING_WINDOW_WEIGHTS := 50 70 90
+
+define extra_sliding-makefiles
+EXTRA_SLIDING_WINDOW_WEIGHT := $(1)
+include $(EXTRA_SLIDING_WINDOW_MODULE_NAME)/template.mk
+endef
+
+$(foreach w,$(EXTRA_SLIDING_WINDOW_WEIGHTS),$(eval $(call extra_sliding-makefiles,$(w))))
