aacc3301338b29ecac2d4fb8b5fcd30e6beae498
Mohammad Agbarya <e.mohammad@gmail.com>
2022-09-12 16:54:08 +0300

    Fix 1GB pages placement to be aligned to allocated 2MB window in extra_sliding_window

--- experiments/layouts_generator.py
+++ experiments/layouts_generator.py
@@ -176,15 +176,16 @@ class LayoutsGenerator:
         step_size = round_up(step_size, 4*kb)
 
         if self._use_1gb_pages:
-            start_1gb_offset = ((self._num_layouts - 1) * step_size) + start_offset + window_length
-            end_1gb_offset = round_up(brk_footprint - start_1gb_offset, 1*gb) + start_1gb_offset
-            brk_footprint = end_1gb_offset
+            last_window_2mb_offset = ((self._num_layouts - 1) * step_size) + start_offset + window_length
 
         for i in range(0, self._num_layouts):
             end_offset = (start_offset + window_length)
             page_size = 2*mb
 
             if self._use_1gb_pages:
+                start_1gb_offset = round_up(last_window_2mb_offset - end_offset, 2*mb) + end_offset
+                end_1gb_offset = round_up(brk_footprint - start_1gb_offset, 1*gb) + start_1gb_offset
+                brk_footprint = end_1gb_offset
                 self.addSingleWindowLayout(brk_start_2m=start_offset, brk_end_2m=end_offset, brk_start_1g=start_1gb_offset, brk_end_1g=end_1gb_offset)
             else:
                 self.addSingleWindowLayout(brk_start_2m=start_offset, brk_end_2m=end_offset)
