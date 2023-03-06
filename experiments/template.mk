817d680d1a831c4e9ecc94f727780886ea5e2f28
Mohammad Agbarya <e.mohammad@gmail.com>
2023-03-06 16:35:20 +0200

    Export Slurm num_threads to NUMBER_OF_THREADS environment variable

--- experiments/template.mk
+++ experiments/template.mk
@@ -14,6 +14,10 @@ ifndef NUM_OF_REPEATS
 NUM_OF_REPEATS := $(DEFAULT_NUM_OF_REPEATS)
 endif # ifndef NUM_OF_REPEATS
 
+ifndef NUMBER_OF_THREADS
+NUMBER_OF_THREADS ?= NUMBER_OF_CORES_PER_SOCKET
+endif
+
 $(MODULE_NAME)% : NUM_LAYOUTS := $(NUM_LAYOUTS)
 $(MODULE_NAME)% : NUM_OF_REPEATS := $(NUM_OF_REPEATS)
 $(MODULE_NAME)%: EXTRA_ARGS_FOR_MOSALLOC := $(EXTRA_ARGS_FOR_MOSALLOC)
@@ -55,7 +59,7 @@ endef
 define SLURM_EXPS_template =
 $(EXPERIMENT_DIR)/$(1)/$(2)/perf.out: %/$(2)/perf.out: $(EXPERIMENT_DIR)/layouts/$(1).csv | experiments-prerequisites 
 	echo ========== [INFO] start producing: $$@ ==========
-	$$(RUN_BENCHMARK_WITH_SLURM) --num_threads=$$(NUMBER_OF_CORES_PER_SOCKET) --num_repeats=$$(NUM_OF_REPEATS) \
+	$$(RUN_BENCHMARK_WITH_SLURM) --num_threads=$$(NUMBER_OF_THREADS) --num_repeats=$$(NUM_OF_REPEATS) \
 		--pre_run_script=$$(PRE_RUN_SCRIPT_NAME) --post_run_script=$$(POST_RUN_SCRIPT_NAME) \
 		--submit_command "$$(MEASURE_GENERAL_METRICS)  \
 		$$(RUN_MOSALLOC_TOOL) --library $$(MOSALLOC_TOOL) -cpf $$(ROOT_DIR)/$$< $$(EXTRA_ARGS_FOR_MOSALLOC)" -- \
