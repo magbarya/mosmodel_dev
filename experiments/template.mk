0f7b200f2273d94d8d0e69d69d087d2049b463a3
Mohammad Agbarya <e.mohammad@gmail.com>
2023-08-18 05:24:02 +0300

    Re-enale outliers detection

--- experiments/template.mk
+++ experiments/template.mk
@@ -80,7 +80,7 @@ $(MEAN_RESULTS): LAYOUT_LIST := $(call array_to_comma_separated,$(LAYOUTS))
 $(MEAN_RESULTS): results/%/mean.csv: experiments/%
 	mkdir -p $(dir $@)
 	$(COLLECT_RESULTS) --experiments_root=$< --repeats=$(NUM_OF_REPEATS) \
-		--layouts=$(LAYOUT_LIST) --output_dir=$(dir $@) --skip_outliers
+		--layouts=$(LAYOUT_LIST) --output_dir=$(dir $@) --remove_outliers
 
 DELETED_TARGETS := $(EXPERIMENTS) $(EXPERIMENT_REPEATS) $(LAYOUTS_DIR)
 CLEAN_TARGETS := $(addsuffix /clean,$(DELETED_TARGETS))
