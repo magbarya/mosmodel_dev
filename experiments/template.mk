e775ab35540a7182d8915c7c90f78398588712f2
Mohammad Agbarya <e.mohammad@gmail.com>
2023-08-22 18:58:05 +0300

    Allocate hugepages before start running the experiment to exclude its time from our measurements

--- experiments/template.mk
+++ experiments/template.mk
@@ -50,6 +50,8 @@ $(EXPERIMENT_REPEATS): %: %/perf.out
 
 define MEASUREMENTS_template =
 $(EXPERIMENT_DIR)/$(1)/$(2)/perf.out: %/perf.out: $(EXPERIMENT_DIR)/layouts/$(1).csv | experiments-prerequisites 
+	echo ========== [INFO] allocate/reserve hugepages ==========
+	$$(SET_CPU_MEMORY_AFFINITY) $$(BOUND_MEMORY_NODE) $$(RUN_MOSALLOC_TOOL) --library $$(MOSALLOC_TOOL) -cpf $$(ROOT_DIR)/$$< /bin/date
 	echo ========== [INFO] start producing: $$@ ==========
 	$$(RUN_BENCHMARK) --num_threads=$$(NUMBER_OF_THREADS) \
 		--submit_command \
@@ -60,6 +62,12 @@ endef
 
 define SLURM_EXPS_template =
 $(EXPERIMENT_DIR)/$(1)/$(2)/perf.out: %/$(2)/perf.out: $(EXPERIMENT_DIR)/layouts/$(1).csv | experiments-prerequisites 
+	echo ========== [INFO] allocate/reserve hugepages ==========
+	for ((i=0; i < $$(NUMBER_OF_SOCKETS); i++)) do
+		srun -- $$(RUN_MOSALLOC_TOOL) --library $$(MOSALLOC_TOOL) -cpf $$(ROOT_DIR)/$$< sleep 2 &
+	done
+	wait
+	echo ========== [INFO] hugepages allocation is done for all nodes ==========
 	echo ========== [INFO] start producing: $$@ ==========
 	$$(RUN_BENCHMARK_WITH_SLURM) --num_threads=$$(NUMBER_OF_THREADS) --num_repeats=$$(NUM_OF_REPEATS) \
 		--submit_command "$$(MEASURE_GENERAL_METRICS)  \
