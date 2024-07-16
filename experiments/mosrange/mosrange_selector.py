8919a20b38ec008bcab2bafbdb5c49a79da9da36
Mohammad Agbarya <e.mohammad@gmail.com>
2024-07-16 13:48:18 +0300

    Mosrange: Fix metric_val and metric_coverage update after running first layout

--- experiments/mosrange/mosrange_selector.py
+++ experiments/mosrange/mosrange_selector.py
@@ -1012,6 +1012,7 @@ class MosrangeSelector(Selector):
         # update metric_coverage to the one got by the executed layout to save convergence time
         real_coverage = self.realMetricCoverage(layout_result)
         self.metric_coverage = real_coverage
+        self.metric_val = None
         self.update_metric_values()
 
         self.logger.info("=====================================================")
