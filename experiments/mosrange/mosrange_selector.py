90fe178af337c61d53da998522877575ef82eeb4
Mohammad Agbarya <e.mohammad@gmail.com>
2024-07-29 18:05:23 +0300

    Consider max_iterations constraint in binary_search_tail_pages_selector before and after recursion call

--- experiments/mosrange/mosrange_selector.py
+++ experiments/mosrange/mosrange_selector.py
@@ -299,6 +299,8 @@ class MosrangeSelector(Selector):
                 return
             if not left_in_range:
                 search(left, mid)
+            if iterations > max_iterations:
+                return
 
             right_in_range = evaluate_subset(right_subset, zero_pages)
             if right_in_range:
@@ -308,6 +310,8 @@ class MosrangeSelector(Selector):
                 return
             if not right_in_range:
                 search(mid, right)
+            if iterations > max_iterations:
+                return
 
             if left_in_range and right_in_range:
                 return
