1272a01fcfedddcddbedd45e702d4be009786f7a
Mohammad Agbarya <e.mohammad@gmail.com>
2024-09-01 09:24:35 +0300

    Fix manual_layouts experiment makefiles

--- experiments/manual_layouts/module.mk
+++ experiments/manual_layouts/module.mk
@@ -4,11 +4,15 @@ MODULE_NAME := experiments/$(MANUAL_LAYOUTS_EXPERIMENT_NAME)
 MANUAL_LAYOUTS_EXPERIMENT := $(MODULE_NAME)
 MANUAL_LAYOUTS_RESULTS := $(subst experiments,results,$(MANUAL_LAYOUTS_EXPERIMENT))
 
-MANUAL_LAYOUTS_DIR := $(MANUAL_LAYOUTS_EXPERIMENT_NAME)/layouts
+MANUAL_LAYOUTS_DIR := $(MANUAL_LAYOUTS_EXPERIMENT)/layouts
 # Gather all CSV files in the layouts directory and remove the .csv suffix
 MANUAL_LAYOUTS := $(patsubst %.csv, %, $(wildcard $(MANUAL_LAYOUTS_DIR)/*.csv))
+MANUAL_LAYOUTS := $(patsubst $(MANUAL_LAYOUTS_EXPERIMENT)/layouts/%, %, $(MANUAL_LAYOUTS))
 # Count the number of layout files
 MANUAL_NUM_LAYOUTS := $(words $(LAYOUTS))
+
+override undefine NUM_LAYOUTS
+override undefine LAYOUTS
 
 NUM_LAYOUTS := $(MANUAL_NUM_LAYOUTS)
 LAYOUTS := $(MANUAL_LAYOUTS)
