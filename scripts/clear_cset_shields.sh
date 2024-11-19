951de6b887800842ad5603fd4b491ff34e7f928e
Mohammad Agbarya <e.mohammad@gmail.com>
2024-11-19 21:27:30 +0200

    Don't skip first core in the node in cset-shield

--- scripts/clear_cset_shields.sh
+++ scripts/clear_cset_shields.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo cset shield --reset
 # Clear all existing configurations
 sudo cset shield --reset --userset=node0_cset;
 
--- scripts/cset_shield_node_cpus.sh
+++ scripts/cset_shield_node_cpus.sh
@@ -4,7 +4,8 @@ NODE=$1
 
 echo " ======> Start configuring cset-shield for memory node ${NODE} <======"
 
-NODE_CPUS=$(cat /sys/devices/system/node/node$NODE/cpulist | cut -d , -f 2-)
+# NODE_CPUS=$(cat /sys/devices/system/node/node$NODE/cpulist | cut -d , -f 2-)
+NODE_CPUS=$(cat /sys/devices/system/node/node$NODE/cpulist)
 
 # cset-shield node CPUs
 sudo cset shield --reset
@@ -12,7 +13,7 @@ sudo cset shield -c ${NODE_CPUS}
 # bind the cset set to local memory node
 echo ${NODE} | sudo tee /sys/fs/cgroup/cpuset/user/cpuset.mems
 # kernel-threads
-sudo cset shield -k on 
+sudo cset shield -k on
 # enable shielding
 sudo cset shield
 
