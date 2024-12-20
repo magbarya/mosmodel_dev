f9e2e3cc1dbd166c58fd158e1236a7d344a5f79f
Mohammad Agbarya <e.mohammad@gmail.com>
2024-12-20 14:02:54 +0200

    Allocate hugepages before start running the benchmark + Add additional settings to set_cpu_max_perf

--- experiments/template.mk
+++ experiments/template.mk
@@ -39,12 +39,17 @@ endef
 
 define CSET_SHIELD_EXPS_template =
 $(EXPERIMENT_DIR)/$(1)/$(2)/perf.out: %/$(2)/perf.out: $(EXPERIMENT_DIR)/layouts/$(1).csv | experiments-prerequisites 
+	echo ========== [INFO] reserve hugepages before start running: $$@ ==========
+	sudo -E cset shield --exec \
+		$$(RUN_WITH_CONDA) -- $$(MEASURE_GENERAL_METRICS) \
+		$$(RUN_MOSALLOC_TOOL) --library $$(MOSALLOC_TOOL) -cpf $$(ROOT_DIR)/$$< $$(EXTRA_ARGS_FOR_MOSALLOC) \
+		-- sleep 1
 	echo ========== [INFO] start producing: $$@ ==========
 	$$(RUN_BENCHMARK_WITH_CSET_SHIELD) \
 		--num_threads=$$(NUMBER_OF_THREADS) \
 		--num_repeats=$$(NUM_OF_REPEATS) \
 		--submit_command "$$(RUN_WITH_CONDA) -- $$(MEASURE_GENERAL_METRICS)  \
-			$$(RUN_MOSALLOC_TOOL) --library $$(MOSALLOC_TOOL) -cpf $$(ROOT_DIR)/$$< $$(EXTRA_ARGS_FOR_MOSALLOC)" \
+			$$(RUN_MOSALLOC_TOOL) --library $$(MOSALLOC_TOOL) -cpf $$(ROOT_DIR)/$$< $$(EXTRA_ARGS_FOR_MOSALLOC) --debug" \
 			-- $$(BENCHMARK_PATH) $$*
 endef
 
--- scripts/set_cpu_max_perf.sh
+++ scripts/set_cpu_max_perf.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+# disable intel p-states: we don't apply this as we observed weird behavior where the CPU runs with the min frequency
+# echo "Disable Intel P-States"
+# echo off | sudo tee /sys/devices/system/cpu/intel_pstate/status
+
 # Set CPU frequency to the maximum for all CPUs
 echo "Setting CPU frequency to maximum..."
 sudo cpupower frequency-set --governor performance
 # sudo cpupower frequency-set --max `cpupower frequency-info -l | grep -oP '\d+\.\d+' | sort -n | tail -1`
 sudo cpupower frequency-set --max `cpupower frequency-info -l | grep -oP '\d+\.?\d*' | sort -n | tail -1`
+for cpu in /sys/devices/system/cpu/cpu*; do
+    echo performance | sudo tee "$cpu/cpufreq/scaling_governor"
+done
+
+# Disable Transparent Huge Pages (THP)
+echo "Disable Transparent Huge Pages (THP)"
+echo never | sudo tee /sys/kernel/mm/transparent_hugepage/enabled
+echo never | sudo tee /sys/kernel/mm/transparent_hugepage/defrag
 
 # Disable automatic NUMA balancing
 echo "Disabling automatic NUMA balancing..."
@@ -20,6 +32,29 @@ for cpu in /sys/devices/system/cpu/cpu*/cpuidle/state[1-3]/disable; do
     echo 1 | sudo tee $cpu;
 done
 
-echo "All operations completed to set CPUs operating in max performance."
+# disable intel turbo
+echo "Disable Intel CPU Turbo..."
+echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 
+# drop page cache
+echo "Drop caches..."
+echo 3 | sudo tee /proc/sys/vm/drop_caches
+
+# disable watchdog
+echo "Turn off watchdog..."
+echo 0 | sudo tee /proc/sys/kernel/watchdog
+
+# extend vmstat interval
+echo "Extend vmstat_update interval to 1M seconds"
+echo 1000000 | sudo tee /proc/sys/vm/stat_interval
+
+# disable ASLR
+echo "Disable ASLR"
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+
+# Disable Kernel Samepage Merging (KSM)
+echo "Disable Kernel Samepage Merging (KSM)"
+echo 0 | sudo tee /sys/kernel/mm/ksm/run
+
+echo "All operations completed to set CPUs operating in max performance."
 
